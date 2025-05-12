33ae1cc36fdee40709e9e26fc866c03a9e2d28f8
bbqcream <imground11@gmail.com>
2025-05-12 20:26:33 +0900

    feat :: sidebar

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import Sidebar from "@/components/common/sidebar";
 import React from "react";
 
 const Home = () => {
     return (
         <div className="w-full h-screen flex justify-center items-center">
-
+            <Sidebar />
         </div>
     );
 };
