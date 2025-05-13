94186b205945febf9b75307f55ad888e4e527ad9
bbqcream <imground11@gmail.com>
2025-05-13 19:50:52 +0900

    fix :: sidebar pathname

--- src/components/common/sidebar/index.tsx
+++ src/components/common/sidebar/index.tsx
@@ -1,10 +1,15 @@
+'use client'
+
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return (
         <div className="w-53 h-96 p-4 bg-white flex flex-col gap-4 rounded-sm">
-            <div className="w-full text-base pl-2 py-2 cursor-pointer bg-second text-white">홈</div>
-            <div className="w-full text-base pl-2 py-2 cursor-pointer bg-white text-black">급식표</div>
+            <div className="w-full text-base pl-2 py-2 cursor-pointer" 
+            style={pathname === "/" ? {backgroundColor : "#FE6B4B", color : "white"} : {background:"white", color:"#303030"}}>홈</div>
+            <div className="w-full text-base pl-2 py-2 cursor-pointer bg-white text-black">급식표</div> {/*이거도 파라미터 값 정하면 위에 홈처럼*/}
             <div className="w-full text-base pl-2 py-2 cursor-pointer bg-white text-black">잔반 통계</div>
             <div className="w-full text-base pl-2 py-2 cursor-pointer bg-white text-black">식사 통계</div>
             <div className="w-full text-base pl-2 py-2 cursor-pointer bg-white text-black">알러지</div>
