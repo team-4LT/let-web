8a2de70f04df1ec4cdca9a1416fa129867877bc6
bbqcream <imground11@gmail.com>
2025-05-27 17:38:26 +0900

    fix :: checktable

--- src/types/props/statistics/checkButtonProps.ts
+++ src/types/props/statistics/checkButtonProps.ts
@@ -1,5 +1,6 @@
 export interface CheckButtonProps {
-  id: 0 | 1 | 2 | 3; // 학년별
-  checkNum: 0 | 1 | 2 | 3;
-  setCheckNum: (id: 0| 1 | 2 | 3) => void;
+    id: number; // 0 : 1학년, 1 : 2학년, 2 : 3학년, 3 : 전체
+    // 0 : 아침, 1 : 점심, 2 : 저녁
+    checkNum: number;
+    setCheckNum: (id: number) => void;
 }
