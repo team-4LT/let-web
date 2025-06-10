bba8ceb9638e69716a1a0f7a06a4a00135de657b
bbqcream <imground11@gmail.com>
2025-06-10 09:41:47 +0900

    fix :: function error

--- src/components/common/home/FeedbackTable.tsx
+++ src/components/common/home/FeedbackTable.tsx
@@ -1,12 +1,17 @@
 const Feedbacktable = () => {
-  return (
-    <div className='w-fill p-5 h-72 bg-white flex flex-col rounded-sm'>
-      <div className='font-semibold text-black text-base'>4월 식단 피드백</div>
-      <div className='w-fill text-darkgrey py-6 text-sm font-normal'>
-            전반적으로 균형 잡힌 식단이에요. 채소 섭취도 충분해서 비타민과 섬유질도 잘 챙기셨어요. 아침 식사가 다소 부족해 보이므로 단백질(예: 삶은 달걀, 두유 등)을 추가하면 더 좋을 것 같아요. 간식으로 과일을 선택하신 점은 좋지만, 양 조절을 신경 써 주세요.
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="w-fill p-5 h-72 bg-white flex flex-col rounded-sm">
+            <div className="font-semibold text-black text-base">
+                4월 식단 피드백
+            </div>
+            <div className="w-fill text-darkgrey py-6 text-sm font-normal">
+                전반적으로 균형 잡힌 식단이에요. 채소 섭취도 충분해서 비타민과
+                섬유질도 잘 챙기셨어요. 아침 식사가 다소 부족해 보이므로
+                단백질(예: 삶은 달걀, 두유 등)을 추가하면 더 좋을 것 같아요.
+                간식으로 과일을 선택하신 점은 좋지만, 양 조절을 신경 써 주세요.
+            </div>
+        </div>
+    );
+};
 
-export default Feedbacktable
+export default Feedbacktable;
--- src/components/common/home/StatusTable.tsx
+++ src/components/common/home/StatusTable.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import { useMealTime } from "@/hooks/mealbar/useMealTime";
+import { useCustomRouter } from "@/hooks/useCustomRouter";
 
 const Statustable = () => {
     const { condition } = useMealTime();
+    const router = useCustomRouter();
     return (
         <div className="w-fill h-35 p-5 bg-white flex flex-col rounded-sm">
             <div className="w-fill flex justify-between items-center">
@@ -9,7 +13,10 @@ const Statustable = () => {
                     식사자 현황
                 </div>
                 <div className="flex items-center gap-1.5">
-                    <div className="text-grey font-semibold text-xs cursor-pointer">
+                    <div
+                        className="text-grey font-medium text-xs cursor-pointer"
+                        onClick={() => router.push("/statistics")}
+                    >
                         바로 가기
                     </div>
                     <img src="/assets/arrow.svg" className="w-2" />
@@ -18,9 +25,7 @@ const Statustable = () => {
             <div className="text-center text-darkgrey text-sm py-6">
                 {condition}을 거른 사람이
                 <br />
-                <span className="font-bold">
-                    총 85명{" "}
-                </span>
+                <span className="font-bold">총 85명 </span>
                 있습니다.
             </div>
         </div>
