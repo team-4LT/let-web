c4b76f4fee1bbea4d08c7ede77b948038e1ff381
bbqcream <imground11@gmail.com>
2025-06-19 11:48:33 +0900

    fix :: login structure

--- src/components/common/auth/LoginButton.tsx
+++ src/components/common/auth/LoginButton.tsx
@@ -1,22 +1,26 @@
+import { useLogin } from "@/hooks/auth/useLogin";
 import { LoginButtonProps } from "@/types/props/auth/loginButtonProps";
 
 const LoginButton = ({ title, condition }: LoginButtonProps) => {
-  return (
-    <div
-      className={`w-full flex items-center justify-center py-2 h-10 text-white rounded-sm cursor-pointer text-base ${
-        condition ? "" : "bg-grey"
-      }`}
-      style={
-        condition
-          ? {
-              background: "linear-gradient(180deg, #FF3939 0%, #FF8957 100%)",
+    const { login } = useLogin();
+    return (
+        <div
+            className={`w-full flex items-center justify-center py-2 h-10 text-white rounded-sm cursor-pointer text-base ${
+                condition ? "" : "bg-grey"
+            }`}
+            style={
+                condition
+                    ? {
+                          background:
+                              "linear-gradient(180deg, #FF3939 0%, #FF8957 100%)",
+                      }
+                    : undefined
             }
-          : undefined
-      }
-    >
-      {title}
-    </div>
-  );
+            onClick={() => login()}
+        >
+            {title}
+        </div>
+    );
 };
 
 export default LoginButton;
--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -8,30 +8,36 @@ export const useLogin = () => {
     const [id, setId] = useState<string>("");
     const [pw, setPw] = useState<string>("");
     const [isError, setError] = useState<boolean>(false);
-    
+    const loginData = {
+        username: id,
+        password: pw,
+    };
+
     const login = async () => {
-        const { data } = await axios.post(
-            `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-            {
-                id: id,
-                pw: pw,
+        try {
+            const data = await axios.post(
+                `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+                loginData
+            );
+            if (data) {
+                if (data?.data.res === 200) {
+                    router.push("/");
+                    setError(false);
+                    alert("로그인 완료.");
+                    const newAccessToken = data.data.accessToken;
+                    const newRefreshToken = data.data.refreshToken;
+                    await cookieManager.set("ACCESS_TOKEN_KEY", newAccessToken);
+                    await cookieManager.set(
+                        "REFRESH_TOKEN_KEY",
+                        newRefreshToken
+                    );
+                } else {
+                    console.log("인증 실패", data);
+                    setError(true);
+                }
             }
-        );
-        if (data) {
-            if (data?.data.res === 200) {
-                router.push("/");
-                setError(false);
-                alert("로그인 완료.");
-                const newAccessToken = data.data.accessToken;
-                const newRefreshToken = data.data.refreshToken;
-                await cookieManager.set("ACCESS_TOKEN_KEY", newAccessToken);
-                await cookieManager.set("REFRESH_TOKEN_KEY", newRefreshToken);
-            } else {
-                console.log("인증 실패", data);
-                setError(true);
-            }
-        } else {
-            console.log("데이터가 없습니다.");
+        } catch (error) {
+            console.error(error);
         }
     };
     return { id, setId, pw, setPw, login, isError };
