00196a1ce3af57116423327b1e4535b6c64e8e71
Isoye12 <0150371278i@gmail.com>
2025-06-30 19:29:00 +0900

    feat :: rank api

--- src/components/common/likes/RankBox.tsx
+++ src/components/common/likes/RankBox.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RankBoxProps } from "@/types/props/likes/rankBoxProps"
 import { useGetChangeElement } from '@/hooks/likes/useGetChangeElement';
+import customAxios from '@/libs/axios/customAxios';
 
-const RankBox = ({ rank, menuName, score, changeValue }: RankBoxProps) => {
-  const change = useGetChangeElement(changeValue);
+const RankBox = ({ rank, menuName, score, menuId }: RankBoxProps) => {
+  const [ rankChange, setRankChange ] = useState<number>(0);
+
+  const getChange = async () => {
+    try{
+        const res = await customAxios.get(`${process.env.NEXT_PUBLIC_API_URL}/menu-rank/diff/${menuId}`)
+        if(res){
+            setRankChange(res.data)
+        }
+    }catch(err){
+        console.log(err)
+    }
+  }
+
+  const change = useGetChangeElement(rankChange);
 
   return (
   <div className="flex gap-2.5 px-2.5 py-3.75 text-black text-base">
