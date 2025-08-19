26fa8c30e3dc34edae4660b417a36eab718a8c6a
Isoye12 <0150371278i@gmail.com>
2025-08-19 10:56:07 +0900

    fix :: 유저 이름 불러오기 삭제, 로그아웃 위치 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,21 @@
 "use client";
 import { useRouter } from "next/navigation"; 
+import { cookieManager } from "@/utilities/cookie";
 
 const Header = () => {
     const router = useRouter();
 
+    const toggleLogout = () =>{
+        alert("로그아웃 되었습니다.")
+        cookieManager.delete("ACCESS_TOKEN_KEY");
+        cookieManager.delete("REFRESH_TOKEN_KEY");
+        router.push("/login")
+    }
+
     return (
         <div className="w-full bg-white border-b border-grey px-24 py-6 flex justify-between items-center fixed">
             <img src="/assets/logo.svg" className="w-17 cursor-pointer" onClick={() => { router.push("/"); }}/>
-            <div className="text-black text-lg font-[500]">밥묵짜 선생님</div>
+            <div className="pl-2 text-main cursor-pointer" onClick={toggleLogout}>로그아웃</div>
         </div>
     );
 };
--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useCustomRouter } from "@/hooks/useCustomRouter";
-import { cookieManager } from "@/utilities/cookie";
 import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
@@ -10,15 +9,9 @@ const Sidebar = () => {
     const condition = (currentPath: string) =>{
         return pathname === currentPath ? {backgroundColor : "#FE6B4B", color : "white"} : {background:"white", color:"#303030"}
     }
-    const toggleLogout = () =>{
-        alert("로그아웃 되었습니다.")
-        cookieManager.delete("ACCESS_TOKEN_KEY");
-        cookieManager.delete("REFRESH_TOKEN_KEY");
-        router.push("/login")
-    }
 
     return (
-        <div className="w-52 h-96 p-4 bg-white flex flex-col gap-4 rounded-sm">
+        <div className="w-52 h-62 p-4 bg-white flex flex-col gap-4 rounded-sm">
             {/* 홈 화면 */}
             <div onClick={()=>{router.push("/");}} className="w-full text-base pl-2 py-2 cursor-pointer" 
             style={condition("/")}>홈</div>
@@ -31,10 +24,6 @@ const Sidebar = () => {
             {/* 급식표 & 알러지 */}
             <div onClick={()=>{router.push("/allergies");}} className="w-full text-base pl-2 py-2 cursor-pointer" 
             style={condition("/allergies")}>급식표 & 알러지</div>
-            
-            <div className="w-full border-t-1 py-4 border-t-grey">
-                <div className="pl-2 text-main cursor-pointer" onClick={toggleLogout}>로그아웃</div>
-            </div>
         </div>
     );
 };
