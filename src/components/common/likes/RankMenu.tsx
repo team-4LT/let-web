74910f5f35d5dfa0769cc190f88750e6bdf26ddb
Isoye12 <0150371278i@gmail.com>
2025-08-20 23:36:04 +0900

    fix :: rank 정렬 안됨 수정

--- src/components/common/likes/RankMenu.tsx
+++ src/components/common/likes/RankMenu.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
-const RankMenu = () => {
+interface RankMenuProps {
+  isAsc: boolean;
+  toggleSort: () => void;
+}
+
+const RankMenu = ({ isAsc, toggleSort }: RankMenuProps) => {
   return (
     <div className="flex gap-2.5 px-2.5 py-3.75 text-black text-base">
-      <div className="flex gap-1.5 w-37.5">
+      <div
+        className="flex gap-1.5 w-37.5 cursor-pointer items-center"
+        onClick={toggleSort} // 클릭 시 정렬 기준 토글
+      >
         <Image
           src="/assets/upDownArrow.svg"
           alt="정렬 바꾸기"
           width={20}
           height={20}
-        /> 
-          순위(응답 기준)
-        </div>
-          <div className="w-62.5">메뉴명</div>
-          <div className="w-50">점수</div>
-          <div className="w-25">변동</div>
+          className={`${isAsc ? "rotate-0" : "rotate-180"} transition-transform`}
+        />
+        순위(응답 기준)
       </div>
-)
-}
+      <div className="w-62.5">메뉴명</div>
+      <div className="w-50">점수</div>
+      <div className="w-25">변동</div>
+    </div>
+  );
+};
 
-export default RankMenu
+export default RankMenu;
