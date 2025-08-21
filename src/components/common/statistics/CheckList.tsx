de473cf3ea5f574a0cd5c5dc85dda61aa408271c
bbqcream <imground11@gmail.com>
2025-08-21 14:11:54 +0900

    fix :: 통계 화면 비율 맞추기

--- src/components/common/statistics/CheckList.tsx
+++ src/components/common/statistics/CheckList.tsx
@@ -3,11 +3,20 @@ import { EaterProps } from "@/types/props/statistics/EaterProps";
 const Checklist = ({ id, name, isEat, grade, number }: EaterProps) => {
     return (
         <div className="w-full flex-col">
-            <div className="w-full flex justify-between border-t border-grey pt-2.5 pb-2.5">
-                <div className="text-black text-base">{grade}반</div>
-                <div className="text-black text-base">{number}번</div>
-                <div className="text-black text-base">{name}</div>
-                <div className="text-base font-medium" style={isEat ? {color:"#888888"} : {color:"#FE6B4B"}}>
+            <div className="w-full flex border-t border-grey pt-2.5 pb-2.5">
+                <div className="flex-1 text-center text-black text-base">
+                    {grade}반
+                </div>
+                <div className="flex-1 text-center text-black text-base">
+                    {number}번
+                </div>
+                <div className="flex-1 text-center text-black text-base">
+                    {name}
+                </div>
+                <div
+                    className="flex-1 text-center text-base font-medium"
+                    style={isEat ? { color: "#888888" } : { color: "#FE6B4B" }}
+                >
                     {isEat ? "식사 완료" : "식사 미완료"}
                 </div>
             </div>
--- src/hooks/allergies/useGetThisMonthCalendar.ts
+++ src/hooks/allergies/useGetThisMonthCalendar.ts
@@ -1,32 +1,35 @@
 // 윤년인지 판단
 const isLeapYear = (year: number): boolean =>
-  (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 
 // 이번 달 정보 가져오기
 export const useGetThisMonthProps = () => {
-  const now = new Date(); // 현재 날짜
-  const month = now.getMonth(); // 현재 월
-  const year = now.getFullYear(); // 현재 연도
+    const now = new Date(); // 현재 날짜
+    const month = now.getMonth(); // 현재 월 (+1 해 줘야함)
+    const year = now.getFullYear(); // 현재 연도
 
-  const daysThisMonth = [] // 이번 달 일들을 담을 배열 (1~31 사이)
-  
-  // 이번 달 마지막 날짜 계산
-  const lastDay = month ===1 // 만약 2월이면 (index 기준 1)
-                ? (isLeapYear(year) ? 29 : 28) // 윤년이면 29일, 아니면 28일
-                : [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month]; // 그 외의 달은 배열에서 해당 월의 일 수를 가져오기
+    const daysThisMonth = []; // 이번 달 일들을 담을 배열 (1~31 사이)
 
-  // 1일부터 마지막 날짜까지 배열에 추가
-  for(let i=1; i<=lastDay; i++){
-    daysThisMonth.push(i) // 해당 월의 마지막 날짜까지 배열에 추가
-  }
+    // 이번 달 마지막 날짜 계산
+    const lastDay =
+        month === 1 // 만약 2월이면 (index 기준 1)
+            ? isLeapYear(year)
+                ? 29
+                : 28 // 윤년이면 29일, 아니면 28일
+            : [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month]; // 그 외의 달은 배열에서 해당 월의 일 수를 가져오기
 
-  // 이번 달 1일의 요일을 구하기 (0: 일요일 ~ 6: 토요일)
-  const startWeek = new Date(year, month, 1).getDay();
+    // 1일부터 마지막 날짜까지 배열에 추가
+    for (let i = 1; i <= lastDay; i++) {
+        daysThisMonth.push(i); // 해당 월의 마지막 날짜까지 배열에 추가
+    }
 
-  return {
-    startWeek,      // 이번 달 1일의 요일
-    daysThisMonth,  // 이번 달 날짜 리스트
-    year,           // 올해 연도
-    month           // 현재 월
-  }
-}
+    // 이번 달 1일의 요일을 구하기 (0: 일요일 ~ 6: 토요일)
+    const startWeek = new Date(year, month, 1).getDay();
+
+    return {
+        startWeek, // 이번 달 1일의 요일
+        daysThisMonth, // 이번 달 날짜 리스트
+        year, // 올해 연도
+        month, // 현재 월
+    };
+};
