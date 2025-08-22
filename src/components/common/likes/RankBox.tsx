6bcee57895423577bee573f6a8f7ed347f7b4e5d
Isoye12 <0150371278i@gmail.com>
2025-08-22 13:49:28 +0900

    fix :: 랭킹 점수 소수점 자르기

--- src/components/common/likes/RankBox.tsx
+++ src/components/common/likes/RankBox.tsx
@@ -27,7 +27,7 @@ const RankBox = ({ rank, menuName, score, menuId }: RankBoxProps) => {
   <div className="flex gap-2.5 px-2.5 py-3.75 text-black text-base">
     <div className="w-37.5">{rank}위</div>
     <div className="w-62.5">{menuName}</div>
-    <div className="w-50">{score}</div>
+    <div className="w-50">{score.toFixed(2)}</div>
     <div className="w-25">{change}</div>
   </div>
 );
